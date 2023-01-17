674256bdfff4255b41612d9d89860d15afcdff7f
Svanyova Adrian <adriansvanyova5@gmail.com>
2023-01-17 23:00:32 +0100

    some progress, will see

--- data.py
+++ data.py
@@ -1,13 +1,5 @@
 class Data:
-    def __init__(self) -> None:
-        self.day: str
-        self.base_hours: float = 0
-        self.thirty_percent: float = 0
-        self.fourty_percent: float = 0
-        self.hundred_percent: float = 0
-        self.plus_hours: float = 0
-        
-    def add(self,day=None, base=0, thirty=0, fourty=0, hundred=0, plus=0):
+    def __init__(self,day=None, base=0, thirty=0, fourty=0, hundred=0, plus=0):
         self.day = day,
         self.base_hours = base
         self.thirty_percent = thirty
--- logics.py
+++ logics.py
@@ -1,15 +1,12 @@
+from data import Data
+
 class Logics:
     def __init__(self) -> None:
         self.age: int
-        self.base_hours = []
-        self.thirty_percent = []
-        self.fourty_percent = []
         self.wage: int
         self.paid_off: int
         self.sick: int
-        self.hundred_percent = []
-        self.plus_hours = []
-        self.date = []
+        self.datas = []
         
     def convert_to_decimal(self, time: str):
         time = time.split(':')
@@ -26,51 +23,47 @@ class Logics:
             if worked_hours > 6 and worked_hours <= 8.75: return 0.75
             
     def counting_hours(self, clk_in: float, clk_out: float, is_double_money: bool):
+        
+        data = Data()
         if clk_in < clk_out:
             hours = clk_out - clk_in
             double_money = 1
             
             if is_double_money:
                 double_money = 2
-                self.hundred_percent.append(hours)
+                data.hundred_percent += hours - self.break_time(hours)
                 
             if clk_in > 0 and clk_out <= 6:
-                self.fourty_percent.append(hours * double_money)
-                self.thirty_percent.append(0)
+                data.fourty_percent += hours * double_money
     
-            elif clk_in > 22 and clk_out <= 24:
-                self.fourty_percent.append(hours * double_money)
-                self.thirty_percent.append(0)
+            elif clk_in > 22 and (clk_out <= 24 or clk_out == 0):
+                data.fourty_percent += hours * double_money
                 
             elif clk_in > 18 and clk_out <= 22:
-                self.thirty_percent.append(hours * double_money)
-                self.fourty_percent.append(0)
+                data.thirty_percent += hours * double_money
                 
             elif clk_in > 0 and clk_in < 6 and clk_out <= 18 and clk_out > 6:
-                self.fourty_percent.append((6 - clk_in) * double_money)
-                self.thirty_percent.append(0)
+                data.fourty_percent += (6 - clk_in) * double_money
                 
             elif clk_in > 6 and clk_out <= 22 and clk_out > 18:
-                self.thirty_percent.append((clk_out - 18) * double_money)
-                self.fourty_percent.append(0)
+                data.thirty_percent += (clk_out - 18) * double_money
             
             elif clk_in > 18 and (clk_out <= 24 or clk_out == 0) and clk_out > 22:
-                self.fourty_percent.append((6 - clk_in) * double_money)
-                self.thirty_percent.append((clk_out - 18) * double_money)
+                data.fourty_percent += (6 - clk_in) * double_money
+                data.thirty_percent += (clk_out - 18) * double_money
             
             elif clk_in > 0 and clk_in <= 6 and clk_out < 22 and clk_out >= 18:
-                self.fourty_percent.append((6 - clk_in) * double_money)
-                self.thirty_percent.append((clk_out - 18) * double_money)
+                data.fourty_percent += (6 - clk_in) * double_money
+                data.thirty_percent += (clk_out - 18) * double_money
             
             elif clk_in > 6 and clk_in <= 18 and (clk_out <= 24 or clk_out == 0) and clk_out > 22:
-                self.fourty_percent.append((clk_out - 22) * double_money)
-                self.thirty_percent.append(4 * double_money)
+                data.fourty_percent += (clk_out - 22) * double_money
+                data.thirty_percent += 4 * double_money
+                
             else:
-                self.thirty_percent.append(0)
-                self.fourty_percent.append(0)
-            self.base_hours.append(hours - self.break_time(hours))
-            if not is_double_money:
-                self.hundred_percent.append(0)
+                data.base_hours += hours - self.break_time(hours)
+                
+            self.datas.append(data)
     
     def counting_money(self):
         self.brutto_money = 0
@@ -78,12 +71,11 @@ class Logics:
         tax = 0
         tb: float
         money_care = 200
-        self.brutto_money += sum(self.base_hours) * self.wage
-        self.brutto_money += sum(self.thirty_percent) * self.wage * 0.3
-        self.brutto_money += sum(self.fourty_percent) * self.wage * 0.4
+        for data in self.datas:
+            self.brutto_money += data.base_hours * self.wage + data.thirty_percent * self.wage * 0.3 + data.fourty_percent * self.wage * 0.4 + data.hundred_percent * self.wage
+    
         self.brutto_money += self.sick * self.wage * 0.7
         self.brutto_money += self.paid_off * self.wage
-        self.brutto_money += sum(self.hundred_percent) * self.wage
         
         if self.age < 25:
             if self.brutto_money > 433_000:
