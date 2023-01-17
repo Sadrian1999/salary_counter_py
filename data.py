0edcd7b8c68fb8e878b3d3af42cf497cec509f6b
Svanyova Adrian <adriansvanyova5@gmail.com>
2023-01-17 22:27:07 +0100

    working on a more efficent data structure

---
+++ data.py
@@ -0,0 +1,7 @@
+class Data:
+    def __init__(self) -> None:
+        self.day: str
+        self.thirty_percent: float
+        self.fourty_percent: float
+        self.hundred_percent: float
+        self.plus_hours: float
